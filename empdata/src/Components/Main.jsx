0a347182f734ab762c2d549ba508dd6549ffafbe
Shubham Verma <94439105+S-hub1996@users.noreply.github.com>
2022-12-01 22:22:43 +0530

    dekho

--- empdata/src/Components/Main.jsx
+++ empdata/src/Components/Main.jsx
@@ -1,81 +1,97 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Checkbox, FormControl, FormControlLabel, FormGroup, FormHelperText, Input, InputLabel, Modal, OutlinedInput, Radio, RadioGroup, TextField } from '@mui/material';
-import { CheckBox } from '@mui/icons-material';
-import { useDispatch, useSelector } from 'react-redux';
-import { getEmployees } from '../Redux/Action';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import CameraIcon from "@mui/icons-material/PhotoCamera";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import {
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormHelperText,
+  Input,
+  InputLabel,
+  Modal,
+  OutlinedInput,
+  Radio,
+  RadioGroup,
+  TextField,
+} from "@mui/material";
+import { CheckBox } from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { getEmployees } from "../Redux/Action";
 // import { Image } from '@mui/material/Image';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
-      tericsoft.com
-      </Link>{' '}
+        tericsoft.com
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
-
 const theme = createTheme();
 const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 export default function Main() {
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-    const dispatch = useDispatch();
-    const Emp = useSelector((store) => store.employees);
-   
-    React.useEffect(() => {
-        if ( Emp.length === 0) {
-    
-          dispatch(getEmployees())
-        }
-      }, []);
-      console.log(Emp)
-    
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [open2, setOpen2] = React.useState(false);
+  const handleOpen2 = () => setOpen2(true);
+  const handleClose2 = () => setOpen2(false);
+  const dispatch = useDispatch();
+  const Emp = useSelector((store) => store.employees);
+
+  React.useEffect(() => {
+    if (Emp.length === 0) {
+      dispatch(getEmployees());
+    }
+  }, []);
+  console.log(Emp);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <AppBar position="relative">
-        <Toolbar >
-          <img src='https://tericsoft.com/wp-content/uploads/2021/08/Web-Logo-371x90px-200x49.png' alt='...'  />
-         
+        <Toolbar>
+          <img
+            src="https://tericsoft.com/wp-content/uploads/2021/08/Web-Logo-371x90px-200x49.png"
+            alt="..."
+          />
         </Toolbar>
       </AppBar>
       <main>
         {/* Hero unit */}
         <Box
           sx={{
-            bgcolor: 'background.paper',
+            bgcolor: "background.paper",
             pt: 8,
             pb: 6,
           }}
@@ -90,8 +106,16 @@ export default function Main() {
             >
               Employee's Details
             </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-            An employee is a term for workers and managers working for a company, organization or community. These people are the staff of the organization. In general, any person hired by an employer to do a particular job in exchange for payment is an employee.
+            <Typography
+              variant="h5"
+              align="center"
+              color="text.secondary"
+              paragraph
+            >
+              An employee is a term for workers and managers working for a
+              company, organization or community. These people are the staff of
+              the organization. In general, any person hired by an employer to
+              do a particular job in exchange for payment is an employee.
             </Typography>
             <Stack
               sx={{ pt: 4 }}
@@ -99,51 +123,77 @@ export default function Main() {
               spacing={2}
               justifyContent="center"
             >
-              <Button onClick={handleOpen} variant="contained">Add New Employee</Button>
+              <Button onClick={handleOpen} variant="contained">
+                Add New Employee
+              </Button>
               <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-        <FormControl>
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={style}>
+                  <FormControl>
+                    <Typography>Name</Typography>
+                    <TextField required />
+                    <Typography>Email</Typography>
+                    <TextField required type={"email"} />
+                    <Typography>Phone</Typography>
+                    <TextField required type={"number"} />
+                    <Typography>Date of Birth</Typography>
+                    <OutlinedInput
+                      required
+                      id="dob"
+                      type="date"
+                      variant="outline"
+                    />
+                    {/* <input type={'date'}></input> */}
+                    <Typography>Gender</Typography>
+                    <RadioGroup
+                      required
+                      aria-labelledby="demo-radio-buttons-group-label"
+                      defaultValue="male"
+                      name="radio-buttons-group"
+                    >
+                      <FormControlLabel
+                        value="female"
+                        control={<Radio />}
+                        label="Female"
+                      />
+                      <FormControlLabel
+                        value="male"
+                        control={<Radio />}
+                        label="Male"
+                      />
+                      <FormControlLabel
+                        value="other"
+                        control={<Radio />}
+                        label="Other"
+                      />
+                    </RadioGroup>
+                    <Typography>Hobbies</Typography>
+                    <FormGroup>
+                      <FormControlLabel
+                        control={<Checkbox />}
+                        label=" Playing Cricket"
+                      />
+                      <FormControlLabel
+                        control={<Checkbox />}
+                        label="Watching Movies"
+                      />
+                      <FormControlLabel
+                        control={<Checkbox />}
+                        label="Reading Books"
+                      />
+                    </FormGroup>
 
-  <Typography>Name</Typography>
-  <TextField required/>
-  <Typography>Email</Typography>
-  <TextField required type={'email'}/>
-  <Typography>Phone</Typography>
-  <TextField required type={'number'} />
-  <Typography>Date of Birth</Typography>
-  <OutlinedInput required id='dob' type='date' variant='outline'/>
-  {/* <input type={'date'}></input> */}
-  <Typography>Gender</Typography>
-  <RadioGroup
-  required
-    aria-labelledby="demo-radio-buttons-group-label"
-    defaultValue="male"
-    name="radio-buttons-group"
-
-    >
-    <FormControlLabel value="female" control={<Radio />} label="Female" />
-    <FormControlLabel value="male" control={<Radio />} label="Male" />
-    <FormControlLabel value="other" control={<Radio />} label="Other" />
-  </RadioGroup>
-      <Typography>Hobbies</Typography>
-      <FormGroup>
-  <FormControlLabel control={<Checkbox />} label=" Playing Cricket" />
-  <FormControlLabel control={<Checkbox />} label="Watching Movies" />
-  <FormControlLabel control={<Checkbox />} label="Reading Books" />
-</FormGroup>
-
-  <Button type='submit' variant='contained'> Add</Button>
-
-
-</FormControl>
-        </Box>
-      </Modal>
-              
+                    <Button type="submit" variant="contained">
+                      {" "}
+                      Add
+                    </Button>
+                  </FormControl>
+                </Box>
+              </Modal>
             </Stack>
           </Container>
         </Box>
@@ -153,7 +203,11 @@ export default function Main() {
             {Emp?.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
                 <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
                 >
                   <CardMedia
                     component="img"
@@ -162,24 +216,87 @@ export default function Main() {
                   />
                   <CardContent sx={{ flexGrow: 1 }}>
                     <Typography gutterBottom variant="h5" component="h2">
-                     {card.name}
+                      {card.name}
                     </Typography>
-                    <Typography>
-                   Email: {card.email}
-                    </Typography>
-                    <Typography>
-                    Phone: {card.phone}
-                    </Typography>
-                    <Typography>
-                     Gender: {card.gender}
-                    </Typography>
-                    <Typography>
-                    Hobbies: {card.hobbies}
-                    </Typography>
+                    <Typography>Email: {card.email}</Typography>
+                    <Typography>Phone: {card.phone}</Typography>
+                    <Typography>Gender: {card.gender}</Typography>
+                    <Typography>Hobbies: {card.hobbies}</Typography>
                   </CardContent>
                   <CardActions>
                     <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
+
+                    <Button onClick={handleOpen2} size="small">
+                      Edit
+                    </Button>
+                    <Modal
+                      open={open2}
+                      onClose={handleClose2}
+                      aria-labelledby="modal-modal-title"
+                      aria-describedby="modal-modal-description"
+                    >
+                      <Box sx={style}>
+                        <FormControl>
+                          <Typography>Name</Typography>
+                          <TextField required />
+                          <Typography>Email</Typography>
+                          <TextField required type={"email"} />
+                          <Typography>Phone</Typography>
+                          <TextField required type={"number"} />
+                          <Typography>Date of Birth</Typography>
+                          <OutlinedInput
+                            required
+                            id="dob"
+                            type="date"
+                            variant="outline"
+                          />
+                          {/* <input type={'date'}></input> */}
+                          <Typography>Gender</Typography>
+                          <RadioGroup
+                            required
+                            aria-labelledby="demo-radio-buttons-group-label"
+                            defaultValue="male"
+                            name="radio-buttons-group"
+                          >
+                            <FormControlLabel
+                              value="female"
+                              control={<Radio />}
+                              label="Female"
+                            />
+                            <FormControlLabel
+                              value="male"
+                              control={<Radio />}
+                              label="Male"
+                            />
+                            <FormControlLabel
+                              value="other"
+                              control={<Radio />}
+                              label="Other"
+                            />
+                          </RadioGroup>
+                          <Typography>Hobbies</Typography>
+                          <FormGroup>
+                            <FormControlLabel
+                              control={<Checkbox />}
+                              label=" Playing Cricket"
+                            />
+                            <FormControlLabel
+                              control={<Checkbox />}
+                              label="Watching Movies"
+                            />
+                            <FormControlLabel
+                              control={<Checkbox />}
+                              label="Reading Books"
+                            />
+                          </FormGroup>
+
+                          <Button type="submit" variant="contained">
+                            {" "}
+                            Edit Employee
+                          </Button>
+                        </FormControl>
+                      </Box>
+                    </Modal>
                   </CardActions>
                 </Card>
               </Grid>
@@ -188,7 +305,7 @@ export default function Main() {
         </Container>
       </main>
       {/* Footer */}
-      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+      <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
         <Typography variant="h6" align="center" gutterBottom>
           tericsoft.com
         </Typography>
